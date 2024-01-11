e6fa34cbe48f5b1ed4237da59c1c444f3b43e570
Tahusyad05 <148919713+Tahusyad05@users.noreply.github.com>
2024-01-11 11:24:10 +0700

    Create welcome.sh
    welcome.sh is personalized welcome message for bash terminal with customizable options

---
+++ scripts/welcome.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#when copying the code, ignore text above, its used for make the script executable
+bold=$(tput bold)
+normal=$(tput sgr0)
+current_date=$(date)
+#above me is variables that will be used in the "echo" command
+echo -e "Welcome to terminal ${bold}$USER${normal}!\n Shell youre using right now is located in ${bold}$SHELL${normal}.\n Today is ${bold}$current_date"
+pause
+#copy all of this (whithout hastag) into first line of .bashrc (without the pause command also).
+# this code is optional for you if you want to personalize your terminal when you open it
